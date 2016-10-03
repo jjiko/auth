5d637e0f58a488aee1aca08d8cd5600339b0af84
Joe Jiko <me@joejiko.com>
2016-10-03 09:56:54 -0400

    composer requirements updated

---
+++ src/views/login.blade.php
@@ -0,0 +1,6 @@
+<a href="{{ route('auth.redirect', ['provider' => 'facebook']) }}" class="btn btn-lg btn-primary btn-block facebook"
+   type="submit">Facebook</a>
+<a href="{{ route('auth.redirect', ['provider' => 'twitter']) }}" class="btn btn-lg btn-primary btn-block twitter"
+   type="submit">Twitter</a>
+<a href="{{ route('auth.redirect', ['provider' => 'google']) }}" class="btn btn-lg btn-primary btn-block google"
+   type="submit">Google</a>
