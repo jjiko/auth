f9a8c9d0ef6e559527d73e393433b30a69f462d9
Joe Jiko <me@joejiko.com>
2017-03-12 03:30:53 -0400

    don't use laravel function

--- src/config/services.php
+++ src/config/services.php
@@ -4,7 +4,7 @@ return [
   'google' => [
     'client_id' => env('GOOGLE_ID'),
     'client_secret' => env('GOOGLE_SECRET'),
-    'redirect' => input()->server('HTTP_HOST').'/auth/handler/google'
+    'redirect' => $_SERVER['HTTP_HOST'].'/auth/handler/google'
   ],
   'facebook' => [
 
