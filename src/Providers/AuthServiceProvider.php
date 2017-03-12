3b43ce02c55d2f137cc37e5a85a53f334924a846
Joe Jiko <me@joejiko.com>
2017-03-12 03:15:23 -0400

    revert order

--- src/Providers/AuthServiceProvider.php
+++ src/Providers/AuthServiceProvider.php
@@ -7,11 +7,11 @@ class AuthServiceProvider extends ServiceProvider
   public function boot()
   {
     $this->loadViewsFrom(__DIR__ . '/../views', 'auth');
-    $this->loadRoutesFrom(__DIR__ . '/../Http/routes.php');
   }
 
   public function register()
   {
+    $this->loadRoutesFrom(__DIR__ . '/../Http/routes.php');
     $this->mergeConfigFrom(
       __DIR__ . '/../config/services.php', 'services'
     );
