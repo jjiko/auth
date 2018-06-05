88c87b7c2de5600e604a16db91832dcda030abeb
Joe Jiko <me@joejiko.com>
2018-06-04 21:29:36 -0400

    reformatting

--- src/Http/routes.php
+++ src/Http/routes.php
@@ -1,13 +1,20 @@
 <?php
 Route::group(['namespace' => 'Jiko\Auth\Http\Controllers'], function () {
-  Route::name('auth.register_path')->get('/auth/register', 'AuthController@getRegister');
-  Route::name('auth.login_path')->get('/auth/login', 'AuthController@getLogin');
-  Route::name('auth.logout_path')->get('/auth/logout', 'AuthController@getLogout');
-  Route::name('auth.redirect')->get('/auth/redirect/{provider}', 'AuthController@redirectToProvider');
-  Route::name('auth.connect.redirect')->get('/auth/connection/redirect/{provider}', 'AuthController@redirectToProvider');
-  Route::name('auth.connect.handler')->any('/auth/connection/handler/{provider}', 'AuthController@handleConnectionCallback');
-  Route::name('auth.handler')->get('/auth/handler/{provider}', 'AuthController@handleProviderCallback');
+  Route::prefix('auth')->group(function () {
 
-  Route::name('auth.user_info')->get('/user', 'AuthController@getUser');
-  Route::get('once', 'AuthController@getOnce');
+    Route::name('auth.register_path')->get('register', 'AuthController@getRegister');
+    Route::name('auth.login_path')->get('login', 'AuthController@getLogin');
+    Route::name('auth.logout_path')->get('logout', 'AuthController@getLogout');
+
+    // user account providers
+    Route::name('auth.redirect')->get('redirect/{provider}', 'AuthController@redirectToProvider');
+    Route::name('auth.handler')->get('handler/{provider}', 'AuthController@handleProviderCallback');
+
+    // oauth connections ie. twitter, nest
+    Route::name('auth.connect.redirect')->get('connection/redirect/{provider}', 'AuthController@redirectToProvider');
+    Route::name('auth.connect.handler')->any('connection/handler/{provider}', 'AuthController@handleConnectionCallback');
+  });
+  Route::prefix('user')->group(function () {
+    Route::name('auth.user_info')->get('/', 'AuthController@getUser');
+  });
 });
--- src/Providers/AuthServiceProvider.php
+++ src/Providers/AuthServiceProvider.php
@@ -1,7 +1,12 @@
 <?php namespace Jiko\Auth\Providers;
 
+use Illuminate\Foundation\AliasLoader;
 use Illuminate\Support\ServiceProvider;
 use Jiko\Auth\Commands\MetaCommand;
+use Zizaco\Entrust\EntrustFacade;
+use Zizaco\Entrust\Middleware\EntrustAbility;
+use Zizaco\Entrust\Middleware\EntrustPermission;
+use Zizaco\Entrust\Middleware\EntrustRole;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -15,27 +20,41 @@ class AuthServiceProvider extends ServiceProvider
       ]);
     }
 
+    $this->app['router']->aliasMiddleware('role', EntrustRole::class);
+    $this->app['router']->aliasMiddleware('permission', EntrustPermission::class);
+    $this->app['router']->aliasMiddleware('ability', EntrustAbility::class);
+
     $this->app['auth']->provider('cacheableEloquent',
       function ($app, $config) {
         $config['model']::updated(function ($model) {
           CacheableEloquentUserProvider::clearCache($model);
         });
-        return new CacheableEloquentUserProvider($app['hash'], $config['model']);
+        return new CacheableEloquentUserProvider($app->make('Illuminate\Contracts\Hashing\Hasher'), $config['model']);
       }
     );
   }
 
   public function register()
   {
+
+    $this->app->booting(function () {
+      $loader = AliasLoader::getInstance();
+      $loader->alias(EntrustFacade::class, 'Entrust');
+    });
+
     // Update config
+    $this->mergeConfigFrom(
+      __DIR__ . '/../config/auth.providers.cacheable-users.php', 'auth.providers.cacheable-users'
+    );
+
+    $this->mergeConfigFrom(
+      __DIR__ . '/../config/database.connections.auth.php', 'database.connections.auth'
+    );
     $this->mergeConfigFrom(
       __DIR__ . '/../config/services.php', 'services'
     );
     $this->mergeConfigFrom(
-      __DIR__ . '/../config/auth.providers.users.php', 'auth.providers.users'
-    );
-    $this->mergeConfigFrom(
-      __DIR__ . '/../config/database.connections.php', 'database.connections'
+      __DIR__ . '/../config/entrust.php', 'entrust'
     );
 
     // Routes
